b243f7be18471cfdccb52333d0ceed13a66b671d
Владимир <gulik.v@bk.ru>
2020-02-12 14:42:59 +0300

    Добавлена страница рассрочки

---
+++ app/Controls/InstallmentControl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ */
+class InstallmentControl extends IndexControl {
+    public $pageTitle = "Рассрочка — GASTREET 2020";
+    public $pageTitle_en = "Installment Plan — GASTREET 2020";
+
+    public function render() {
+        $this->controlName = "Рассрочка";
+    }
+}
