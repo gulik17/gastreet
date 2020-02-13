f87a807e3c2442b3367895c8237b809a3cd1c117
Владимир <gulik.v@bk.ru>
2020-02-13 14:14:11 +0300

    Изменение страницы аватар

--- app/avatar/js/main.js
+++ app/avatar/js/main.js
@@ -112,7 +112,8 @@ $(document).ready(function () {
     $('.gss-avatar-choose-ul').owlCarousel({
         margin:10,
         nav:true,
-        dots:false,
+        dots:true,
+        loop:true,
         responsive:{
             0:{items:1},
             600:{items:3}
