adcd62188db7328960bd9ce068dd66a563b763df
Владимир <gulik.v@bk.ru>
2020-02-20 16:29:16 +0300

    Запуск шефской страницы

--- js/app/app.js
+++ js/app/app.js
@@ -729,16 +729,35 @@ $(document).ready(function() {
         dots:true,
         responsive:{
             0:{
-                items:2
+                items:1
             },
             600:{
                 items:2
+            },
+            900:{
+                items:2
+            }
+        }
+    });
+
+    $('#newsCarousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:4
             },
             900:{
                 items:4
             }
         }
     });
+
     $('#spiakerCarousel').owlCarousel({
         loop:true,
         margin:10,
